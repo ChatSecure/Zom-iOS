ee6fc33b883b471447251baf9e4824f89fe64089
Chris Ballinger <chrisballinger@gmail.com>
2015-06-10 13:33:31 -0700

    Add Zom color scheme

--- Whitelabel/Zom/Classes/ZomAppDelegate.m
+++ Whitelabel/Zom/Classes/ZomAppDelegate.m
@@ -10,4 +10,27 @@
 
 @implementation ZomAppDelegate
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    BOOL success = [super application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    [self configureAppearance];
+    
+    return success;
+}
+
+- (void) configureAppearance {
+    UIColor * zomColor = [UIColor colorWithRed:231/255.0f green:39/255.0f blue:90/255.0f alpha:1.0f];
+    UIColor *lighterZomColor = [UIColor colorWithRed:255/255.0f green:221/255.0f blue:230/255.0f alpha:1.0f];
+    [[UINavigationBar appearance] setTranslucent:NO];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UINavigationBar appearance] setBarTintColor:zomColor];
+    [[UINavigationBar appearance] setBackgroundColor:zomColor];
+    [[UINavigationBar appearance] setTitleTextAttributes:@{
+                                                           NSForegroundColorAttributeName: [UIColor whiteColor]
+                                                           }];
+    [[UITableView appearance] setBackgroundColor:lighterZomColor];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+}
+
 @end
